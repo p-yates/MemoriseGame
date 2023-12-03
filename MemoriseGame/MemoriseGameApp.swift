0ddd363aee6243677b918d628f6718b1adee9e60
p-yates <148437369+p-yates@users.noreply.github.com>
2023-12-03 16:36:59 +0100

    Initial Commit

---
+++ MemoriseGame/MemoriseGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriseGameApp.swift
+//  MemoriseGame
+//
+//  Created by Paul Y on 2023-12-03.
+//
+
+import SwiftUI
+
+@main
+struct MemoriseGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
