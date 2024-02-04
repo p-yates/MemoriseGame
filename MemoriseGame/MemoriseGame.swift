98f11751c8e73487c6b1f19dad1e836f29a8b2b0
p-yates <148437369+p-yates@users.noreply.github.com>
2024-02-04 21:57:05 +0100

    put cards back to defaulting face down

--- MemoriseGame/MemoriseGame.swift
+++ MemoriseGame/MemoriseGame.swift
@@ -83,7 +83,7 @@ struct MemoriseGame<CardContent> where CardContent: Equatable {
     
     struct Card: Equatable, Identifiable, CustomDebugStringConvertible {
         
-        var isFaceUp = true
+        var isFaceUp = false
         var isMatched = false
         var hasBeenSeen = false
         let content: CardContent
